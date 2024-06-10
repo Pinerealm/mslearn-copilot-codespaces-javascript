9df33324f7d139fbb5bac9e4fc184696a6f8d88c
Oluwaseyi Salami <osalami09@gmail.com>
2024-06-10 22:52:15 +0000

    chore: Update personal information and social media links
    Update the personal information and social media links in the `siteProps` object in `App.jsx`. Also, add an animation to the social icons in `styles.css`.

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ import "./styles.css";
 
 /**
  * This object represents your information. The project is set so that you
- * only need to update these here, and values are passed a properties to the
+ * only need to update these here, and values are passed as properties to the
  * components that need that information.
  *
  * Update the values below with your information.
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Oluwaseyi Salami",
+  title: "Data Analyst | Software Developer",
+  email: "osalami09@gmail.com",
+  gitHub: "Pinerealm",
+  instagram: "",
+  linkedIn: "osalami",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "pinerealmz",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
